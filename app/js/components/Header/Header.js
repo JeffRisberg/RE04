078f15fdd27d65c5f79de50f6558ac287f4da5ed
JeffRisberg <jrisberg@aol.com>
2017-03-09 22:49:33 +0000

    adjusted css rules to be component-specific

--- app/js/components/Header/Header.js
+++ app/js/components/Header/Header.js
@@ -1,12 +1,9 @@
-import React from 'react'
-import { Link } from 'react-router'
-
-import { connect } from 'react-redux';
-
-import NavLink from '../NavLink'
-import { login, logout } from '../../actions/donor';
-
-import './Header.scss';
+import React from "react";
+import {Link} from "react-router";
+import {connect} from "react-redux";
+import NavLink from "../NavLink";
+import {logout} from "../../actions/donor";
+import "./Header.scss";
 
 /**
  * Appears at top of screen
@@ -39,51 +36,41 @@ class Header extends React.Component {
 
         return (
             <div>
-                <div className="header row white">
-                    <div className="top-border"></div>
+                <div className="header white">
+                    <div className="header__top-border"></div>
                     <div className="container">
-                        <div className="logo col-md-4 col-xs-4">
-                            <img src="images/Logo.png" />
-                        </div>
-
-                        <div className="col-md-8 col-xs-7">
-                            <div className="account-header-links text-right hidden-xs">
-                                <a href="" className="margin-l">Settings</a>
-                                <a href="" className="margin-l">Help</a>
-                                {loginLogout}
+                        <div className="row">
+                            <div className="header__logo col-md-4 col-xs-5">
+                                <img src="images/Logo.png"/>
                             </div>
 
-                            <div className="account-info text-right">
-                                {headerText}
+                            <div className="col-md-8 col-xs-7">
+                                <div className="account-header-links text-right hidden-xs">
+                                    <a href="" className="margin-l">Settings</a>
+                                    <a href="" className="margin-l">Help</a>
+                                    {loginLogout}
+                                </div>
+
+                                <div className="header__account-info text-right">
+                                    {headerText}
+                                </div>
+                            </div>
+                            <div className="col-xs-1 visible-xs-block text-right">
+                                <a id="dropdownMenuLabel" data-toggle="dropdown" data-target="#" href="#"
+                                   aria-expanded="false"
+                                   aria-haspopup="true">
+                                    <span className="glyphicon glyphicon-menu-hamburger"></span>
+                                </a>
+
+                                <ul className="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLabel">
+                                    <li><a href="" className="margin-l">Settings</a></li>
+                                    <li><a href="" className="margin-l">Help</a></li>
+                                    <li><a href="#" className="margin-l">Sign Off</a></li>
+                                </ul>
                             </div>
                         </div>
-                        <div className="col-xs-1 visible-xs-block text-right">
-                            <a id="dropdownMenuLabel" data-toggle="dropdown" data-target="#" href="#"
-                               aria-expanded="false"
-                               aria-haspopup="true">
-                                <span className="glyphicon glyphicon-menu-hamburger"></span>
-                            </a>
-
-                            <ul className="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLabel">
-                                <li><a href="" className="margin-l">Settings</a></li>
-                                <li><a href="" className="margin-l">Help</a></li>
-                                <li><a href="#" className="margin-l">Sign Off</a></li>
-                            </ul>
-                        </div>
                     </div>
-                </div>
-
-                <div className="row darkgrey">
-                    <div className="container">
-                        <ul className="nav nav-pills nav-justified" role="tablist">
-                            <li><a href="#myrewards" aria-controls="myrewards" role="tab" data-toggle="tab">
-                                My Rewards
-                            </a></li>
-                            <li><a href="#earn" aria-controls="earn" role="tab" data-toggle="tab">Earn</a></li>
-                            <li><a href="#use" aria-controls="use" role="tab" data-toggle="tab">Use</a></li>
-                            <li><a href="#share" aria-controls="share" role="tab" data-toggle="tab">Share</a></li>
-                        </ul>
-                    </div>
+                    <div className="header__bottom-border"></div>
                 </div>
 
                 <div className="row" style={{marginBottom: '2px'}}>
@@ -94,10 +81,10 @@ class Header extends React.Component {
                         </div>
                         <div className="col-md-3" style={{textAlign: 'right'}}>
                             <NavLink to="/basket" style={{marginLeft: '10px'}}>
-                                <div className="basket"/>
+                                <div className="header__basket"/>
                             </NavLink>
                             <NavLink to="/givingHistory" style={{marginLeft: '10px'}}>
-                                <div className="giving-history"/>
+                                <div className="header__giving-history"/>
                             </NavLink>
                         </div>
                     </div>
