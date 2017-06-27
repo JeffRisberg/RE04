59b1b64c48210a8d549eef33bd037ae90ef0e8a1
JeffRisberg <jrisberg@aol.com>
2017-06-27 21:27:35 +0100

    Updated versions, fixed use of SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -12,14 +13,15 @@ module.exports = {
         loaders: [
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
+                loader: ExtractTextPlugin.extract('css-loader!sass-loader'),
             },
-            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-            {test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader" },
-            {test: /\.(jpe?g|png|gif|svg)$/i, loader: "file-loader?name=/public/icons/[name].[ext]"}
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+            { test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader" },
+            { test: /\.(jpe?g|png|gif|svg)$/i, loader: "file-loader?name=/public/icons/[name].[ext]" }
         ]
     },
     plugins: [
-        new webpack.IgnorePlugin(/^(buffertools)$/) // unwanted "deeper" dependency
+        new webpack.IgnorePlugin(/^(buffertools)$/), // unwanted "deeper" dependency
+        new ExtractTextPlugin({ filename: 'public/style.css', allChunks: true })
     ]
 };
