9a61671b885240d831f327829148926bc633494c
JeffRisberg <jrisberg@aol.com>
2017-08-24 12:30:25 +0100

    updated use of PropTypes, added intl to Guide

--- app/js/reducers/localeData.js
+++ app/js/reducers/localeData.js
@@ -8,23 +8,6 @@ const initialState = {
   defaultLocale: 'en',
 };
 
-/*
-const locale = (state = initialState, action, _i18n = i18n) => {
-  const enContent = _i18n.en;
-  switch (action.type) {
-    case types.UPDATE_LOCALE: {
-      const localMessages = _i18n[action.locale];
-      return Object.assign({}, state, {
-        locale: action.locale,
-        messages: Object.assign({}, enContent, localMessages),
-      });
-    }
-    default:
-      return state;
-  }
-};
-*/
-
 export default handleActions({
     [types.UPDATE_LOCALE]: (state, action, _i18n = i18n) => {
         const enContent = _i18n.en;
