3c6aac551d64295f1edde5c42d7a5bf5c77a687d
JeffRisberg <jrisberg@aol.com>
2018-05-26 23:58:47 -0700

    changed to use mini-css-text-extract

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        javascript: "./app/js/client.js"
-    },
-    output: {
-        path: __dirname + "/dist",
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract('css-loader!sass-loader'),
-            },
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-            { test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader" },
-            { test: /\.(jpe?g|png|gif|svg)$/i, loader: "file-loader?name=/public/icons/[name].[ext]" }
-        ]
-    },
-    plugins: [
-        new webpack.IgnorePlugin(/^(buffertools)$/), // unwanted "deeper" dependency
-        new ExtractTextPlugin({ filename: 'public/style.css', allChunks: true })
+  mode: 'development',
+  entry: {
+    javascript: "./app/js/client.js"
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [ MiniCssExtractPlugin.loader, "css-loader", "sass-loader" ]
+      },
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.(jpe?g|png|gif|svg)$/i, loader: "file-loader?name=/public/icons/[name].[ext]" }
     ]
+  },
+  plugins: [
+    new webpack.IgnorePlugin(/^(buffertools)$/), // unwanted "deeper" dependency
+    new MiniCssExtractPlugin({ filename: 'public/style.css', allChunks: true })
+  ]
 };
