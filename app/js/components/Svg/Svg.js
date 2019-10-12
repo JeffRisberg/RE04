9d27a2cc739284f2c2b423f2ccc84a9c834517cb
JeffRisberg <jrisberg@aol.com>
2019-10-11 23:15:52 -0700

    updated components

--- app/js/components/Svg/Svg.js
+++ app/js/components/Svg/Svg.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const propTypes = {
-    markup: PropTypes.string.isRequired,
+    markup: PropTypes.object.isRequired,
 };
 
 const Svg = ({ markup, ...props }) => {
